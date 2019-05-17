efff333e5d64028dc803847989753ce5399b7f52
Bec White <white@palantir.net>
2019-05-16 19:04:56 -0500

    Use less opinionated http auth creds

--- .the-build/drupal/settings.build-acquia.php
+++ .the-build/drupal/settings.build-acquia.php
@@ -32,8 +32,8 @@ $cli = (php_sapi_name() == 'cli');
 
 // PASSWORD-PROTECT NON-PRODUCTION SITES (i.e. staging/dev)
 if (!$cli && (isset($_ENV['AH_NON_PRODUCTION']) && $_ENV['AH_NON_PRODUCTION'])) {
-  $username = 'palantir';
-  $password = 'some mediocre password';
+  $username = 'tower';
+  $password = 'health';
   if (!(isset($_SERVER['PHP_AUTH_USER']) && ($_SERVER['PHP_AUTH_USER']==$username && $_SERVER['PHP_AUTH_PW']==$password))) {
     header('WWW-Authenticate: Basic realm="This site is protected"');
     header('HTTP/1.0 401 Unauthorized');
