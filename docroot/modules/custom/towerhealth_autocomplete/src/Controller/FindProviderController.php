4bf79e28bd41deb175e1a5e70f7deed19b30b998
Malak Desai <mdesai13@hawk.iit.edu>
2019-10-02 15:52:25 -0500

    Revert "run code sniffer to fix formatting"
    This reverts commit 334f7532c9696ac4e5e31b6a16dcd5e7f01a3ab6.

--- docroot/modules/custom/towerhealth_autocomplete/src/Controller/FindProviderController.php
+++ docroot/modules/custom/towerhealth_autocomplete/src/Controller/FindProviderController.php
@@ -11,52 +11,48 @@ use Drupal\Component\Utility\Xss;
 /**
  * Defines a route controller for watches autocomplete form elements.
  */
-class FindProviderController extends FindCareController
-{
-    /**
+class FindProviderController extends FindCareController {
+  /**
    * The node storage.
    *
    * @var \Drupal\node\NodeStorage
    */
-    protected $nodeStorage;
+  protected $nodeStorage;
 
-    /**
+  /**
    * {@inheritdoc}
    */
-    public function __construct(EntityTypeManagerInterface $entity_type_manager) 
-    {
-        $this->nodeStroage = $entity_type_manager->getStorage('node');
-    }
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->nodeStroage = $entity_type_manager->getStorage('node');
+  }
 
-    /**
+  /**
    * {@inheritdoc}
    */
-    public static function create(ContainerInterface $container) 
-    {
-        // Instantiates this form class.
-        return new static(
-        $container->get('entity_type.manager')
-        );
-    }
+  public static function create(ContainerInterface $container) {
+    // Instantiates this form class.
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
 
-    /**
+  /**
    * Handler for autocomplete request.
    */
-    public function handleAutocomplete(Request $request) 
-    {
-        $results = [];
-        $input = $request->query->get('q');
-        // Get the typed string from the URL, if it exists.
-        if (!$input) {
-            return new JsonResponse($results);
-        }
-        $input = Xss::filter($input);
-
-        $results = $this->taxonomySuggestedTerms('auto_medical_specialty', $input, t('Medical Specialties'), $results);
-        $results = $this->taxonomySuggestedTerms('auto_condition', $input, t('Conditions'), $results);
-        $results = $this->nodeSuggestedTerms('auto_provider', $input, t('Providers'), $results);
-
-        return new JsonResponse($results);
+  public function handleAutocomplete(Request $request) {
+    $results = [];
+    $input = $request->query->get('q');
+    // Get the typed string from the URL, if it exists.
+    if (!$input) {
+      return new JsonResponse($results);
     }
+    $input = Xss::filter($input);
+
+    $results = $this->taxonomySuggestedTerms('auto_medical_specialty', $input, t('Medical Specialties'), $results);
+    $results = $this->taxonomySuggestedTerms('auto_condition', $input, t('Conditions'), $results);
+    $results = $this->nodeSuggestedTerms('auto_provider', $input, t('Providers'), $results);
+
+    return new JsonResponse($results);
+  }
 
 }
