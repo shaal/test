64dffce7cab5efd44c26092bf70b5e68d04c0d17
Kristen Mayer <mayer@palantir.net>
2019-11-18 18:15:03 -0500

    TH-284 wip side nav component

---
+++ docroot/themes/custom/tower_health/components/_patterns/02-molecules/menus/side-menu/side-menu.js
@@ -0,0 +1,44 @@
+/**
+ * @file
+ * A JavaScript file containing the main menu functionality (small/large screen)
+ *
+ */
+
+// JavaScript should be made compatible with libraries other than jQuery by
+// wrapping it with an "anonymous closure". See:
+// - https://drupal.org/node/1446420
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+
+
+// (function (Drupal) { // UNCOMMENT IF DRUPAL.
+//
+//   Drupal.behaviors.mainMenu = {
+//     attach: function (context) {
+
+(function ($) { // REMOVE IF DRUPAL.
+
+  // Use context instead of document IF DRUPAL.
+  var menu = $('.side-nav li');
+  console.log("loaded");
+  var menu_drawer = $('.side-nav__second');
+
+  // Mobile Menu Show/Hide.
+  function openNav() {
+    menu_drawer.addClass('is-active');
+  }
+
+  function closeNav() {
+    menu_drawer.removeClass('is-active');
+  }
+
+  menu.on('click', function(e) {
+    if ( $(menu_drawer).hasClass('is-active') ) {
+      closeNav();
+    } else {
+      openNav();
+    }
+  });
+
+})(jQuery); // REMOVE IF DRUPAL.
+
+// })(Drupal); // UNCOMMENT IF DRUPAL.
