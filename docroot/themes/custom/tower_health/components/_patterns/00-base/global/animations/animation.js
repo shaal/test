c4e4e3cede4b558975c3000bca8eb99e252fdf0f
Ashley Cyborski <cyborski@palantir.net>
2019-12-02 15:44:54 -0600

    update animations based on new feedback

--- docroot/themes/custom/tower_health/components/_patterns/00-base/global/animations/animation.js
+++ docroot/themes/custom/tower_health/components/_patterns/00-base/global/animations/animation.js
@@ -36,11 +36,6 @@
               (element_top_position <= window_bottom_position)) {
             $element.addClass('in-view');
           }
-          
-          if ((element_bottom_position >= window_top_position) &&
-              (element_middle_position <= window_bottom_position)) {
-            $element.addClass('in-mid-view');
-          }
         });
       }
 
