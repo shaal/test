d3f907e211ce9bc0a03a8c16a401de00d8b062ee
Ashley Cyborski <cyborski@palantir.net>
2019-12-04 13:11:10 -0600

    attach proper libraries, make list filter toggle js, style list filters

---
+++ docroot/themes/custom/tower_health/components/_patterns/03-organisms/listings/listing-filter/listing-filters.js
@@ -0,0 +1,63 @@
+/**
+ * @file
+ * A JavaScript file containing the main menu functionality (small/large screen)
+ *
+ */
+
+// JavaScript should be made compatible with libraries other than jQuery by
+// wrapping it with an "anonymous closure". See:
+// - https://drupal.org/node/1446420
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+
+
+// (function (Drupal) { // UNCOMMENT IF DRUPAL.
+//
+//   Drupal.behaviors.mainMenu = {
+//     attach: function (context) {
+
+(function ($) { // REMOVE IF DRUPAL.
+
+  // Use context instead of document IF DRUPAL.
+  var filter_toggle = $('.filter-_toggle');
+  var filters = $('.filter');
+  var filter_drawer = $('.filter-grid-wrapper');
+
+  // Mobile Menu Show/Hide.
+  function openFilters() {
+    filters.addClass('is-active');
+    filter_drawer.attr("aria-expanded", "true");
+  }
+  
+  function closefilters() {
+    filters.removeClass('is-active');
+    filter_drawer.attr("aria-expanded", "false");
+  }
+  
+  $(window).on('load', function() {
+    if ( $(window).width() < 900 ) {
+      filter_drawer.attr("aria-expanded", "false");
+    } else {
+      filter_drawer.attr("aria-expanded", "true");
+    }
+  });
+    
+  filter_toggle.on('click', function() {
+    if ( $(filters).hasClass('is-active') ) {
+      closefilters();
+    } else {
+      openfilters();
+    }
+  });
+  
+  
+  $(document).keydown(function(event) { 
+    // close menu on esc press
+    if (event.keyCode == 27) { 
+      closefilters();
+    } 
+  });
+  
+
+})(jQuery); // REMOVE IF DRUPAL.
+
+// })(Drupal); // UNCOMMENT IF DRUPAL.
